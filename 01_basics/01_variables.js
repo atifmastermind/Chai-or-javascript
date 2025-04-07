1ec8cdf6f2eaeebb8896e60d1122dc94edd17b4a
Muhammad Atif <84960137+atifmastermind@users.noreply.github.com>
2025-04-07 08:14:42 +0000

    Variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 234435
+let accountEmail = "atifmastermind12@gmail.com"
+var accountPass = "12345"
+accountCity = "karachi"
+let accountState
+
+//accountId = 456456 // TypeError: Assignment to constant variable.
+accountEmail = "atif@gmail.com"
+accountPass = "24242424"
+accountCity = "lahore"
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPass,accountCity,accountState]);
+
